b491d225086cd5a8bd475737485cc03826b87d3f
CodeByBloke <75045738+CodeByBloke@users.noreply.github.com>
2020-12-10 11:40:23 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,30 +8,30 @@
 
 using namespace std;
 
-void outputMenu() {
+void outputMenu() { // Output the main menu
     cout<<"Main Menu:"<<endl;
     cout<<"  1. Play Tic-Tac-Toe"<<endl;
     cout<<"  2. Exit"<<endl<<endl;
     cout<<"Enter Your Selection: ";
 }
 
-void playTicTacToe() {
+void playTicTacToe() { // Play the game
     cout<<"Welcome to Tic-Tac-Toe!"<<endl<<endl;
     cout<<"Determining who will go first..."<<endl;
 
-    int whoseTurn = rand() % 2 + 1;
+    int whoseTurn = rand() % 2 + 1; // Get random number between 1 and 2
     bool usersTurn;
     char userCharacter;
     char computerCharacter;
     Board* board;
-    if(whoseTurn == 1) {
+    if(whoseTurn == 1) { // This condition is for the user going first
         cout<<"  -> You: X's (X's go first)"<<endl;
         cout<<"  -> Computer: O's"<<endl<<endl;
         usersTurn = true;
         userCharacter = 'X';
         computerCharacter = 'O';
         board = new Board(1);
-    } else if(whoseTurn == 2) {
+    } else if(whoseTurn == 2) { // This condiiton is for the computer going first
         cout<<"  -> You: O's"<<endl;
         cout<<"  -> Computer: X's (X's go first)"<<endl<<endl;
         usersTurn = false;
@@ -46,11 +46,12 @@ void playTicTacToe() {
     cout<<"Turn "<<turnNumber<<":"<<endl<<endl;
     board->outputBoard();
     cout<<endl;
-    while(!board->isWinner('X') && !board->isWinner('O') && !board->isGameTied()) {
+    while(!board->isWinner('X') && !board->isWinner('O') && !board->isGameTied()) { // Keep playing while the game is not won loss or tied
         if(usersTurn) {
             int row;
             int column;
 
+            // Get user input
             cout<<"It is your move..."<<endl;
             cout<<"  -> Type which row you want to place your "<<userCharacter<<": ";
             cin >> row;
@@ -67,7 +68,7 @@ void playTicTacToe() {
             cout<<"It is the computer's turn..."<<endl;
             cout<<"The computer is thinking..."<<endl;
 
-            vector<int> move = computer.makeMove();
+            vector<int> move = computer.makeMove(); // Get computer move
             int row = move[0];
             int column = move[1];
 
@@ -82,7 +83,7 @@ void playTicTacToe() {
         }
     }
 
-    bool userWon = board->isWinner(userCharacter);
+    bool userWon = board->isWinner(userCharacter); // Check to see who won
     bool computerWon = board->isWinner(computerCharacter);
 
     if(userWon) {
@@ -115,5 +116,6 @@ int main() {
         cin >> userSelection;
     }
 }
+
 
 
